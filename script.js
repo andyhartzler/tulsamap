776feebb882e7b1e0bd0c16c4b256b158ba36bc5
andyhartzler <70266044+andyhartzler@users.noreply.github.com>
2023-03-03 05:16:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,109 @@
-// Initialize and add the map
+// Define the council member data
+var councilMembers = [
+  {
+    district: "District 1",
+    name: "Councilor Vanessa Hall-Harper",
+    picture: "https://www.cityoftulsa.org/ImageRepository/Document?documentID=27481",
+    email: "vhallharper@tulsacouncil.org",
+    phone: "918-596-1926"
+  },
+  {
+    district: "District 2",
+    name: "Councilor Jeanette Ward",
+    picture: "https://www.cityoftulsa.org/ImageRepository/Document?documentID=27485",
+    email: "jward@tulsacouncil.org",
+    phone: "918-596-1922"
+  },
+  {
+    district: "District 3",
+    name: "Councilor Crista Patrick",
+    picture: "https://www.cityoftulsa.org/ImageRepository/Document?documentID=27487",
+    email: "cpatrick@tulsacouncil.org",
+    phone: "918-596-1923"
+  },
+  {
+    district: "District 4",
+    name: "Councilor Kara Joy McKee",
+    picture: "https://www.cityoftulsa.org/ImageRepository/Document?documentID=27489",
+    email: "kmckee@tulsacouncil.org",
+    phone: "918-596-1964"
+  },
+  {
+    district: "District 5",
+    name: "Councilor Cass Fahler",
+    picture: "https://www.cityoftulsa.org/ImageRepository/Document?documentID=28920",
+    email: "cfahler@tulsacouncil.org",
+    phone: "918-596-1925"
+  },
+  {
+    district: "District 6",
+    name: "Councilor Connie Dodson",
+    picture: "https://www.cityoftulsa.org/ImageRepository/Document?documentID=27483",
+    email: "cdodson@tulsacouncil.org",
+    phone: "918-596-1926"
+  },
+  {
+    district: "District 7",
+    name: "Councilor Lori Decter Wright",
+    picture: "https://www.cityoftulsa.org/ImageRepository/Document?documentID=27484",
+    email: "ldecterwright@tulsacouncil.org",
+    phone: "918-596-1921"
+  },
+  {
+    district: "District 8",
+    name: "Councilor Phil Lakin Jr.",
+    picture: "https://www.cityoftulsa.org/ImageRepository/Document?documentID=27486",
+    email: "plakin@tulsacouncil.org",
+    phone: "918-596-1924"
+  },
+  {
+    district: "District 9",
+    name: "Councilor Ben Kimbro",
+    picture: "https://www.cityoftulsa.org/ImageRepository/Document?documentID=27488",
+    email: "bkimbro@tulsacouncil.org",
+    phone: "918-596-1927"
+  }
+];
+
+// Define the district boundary data
+var districtBoundariesUrl = "map.json";
+
+// Initialize the map
+var map;
+
 function initMap() {
-  // Set the coordinates for the center of the map
-  const centerCoords = { lat: 36.1539, lng: -95.9925 };
-  // Create a new map instance
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 11,
-    center: centerCoords,
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 36.1539823, lng: -95.992775 },
+    zoom: 10
   });
 
-  // Load the JSON data
-  fetch("map.json")
-    .then((response) => response.json())
-    .then((data) => {
-      // Loop through each feature in the GeoJSON data
-      data.features.forEach((feature) => {
-        // Extract the district number and name from the properties
-        const district = feature.properties.DISTRICT;
-        const name = feature.properties.NAME;
+  // Load the district boundaries
+  var districtBoundariesLayer = new google.maps.Data();
+  districtBoundariesLayer.loadGeoJson(districtBoundariesUrl);
+  districtBoundariesLayer.setMap(map);
 
-        // Create a new polygon for the district
-        const districtPolygon = new google.maps.Polygon({
-          paths: feature.geometry.coordinates,
-          strokeColor: "#FF0000",
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: "#FF0000",
-          fillOpacity: 0.35,
-        });
-
-        // Add a click event listener to the polygon
-        districtPolygon.addListener("click", () => {
-          // Show an info window with the district name
-          const infoWindow = new google.maps.InfoWindow({
-            content: `<h2>${name}</h2>`,
-          });
-          infoWindow.open(map, districtPolygon);
-        });
-
-        // Set the polygon on the map
-        districtPolygon.setMap(map);
-      });
-    });
+  // Add a click listener to the district boundaries layer
+  districtBoundariesLayer.addListener("click", function(event) {
+    var districtName = event.feature.getProperty("NAME");
+    showCouncilMemberInfo(districtName);
+  });
 }
+
+// Define the function to show council member info
+function showCouncilMemberInfo(district) {
+  // Find the council member for the selected district
+  var councilMember = councilMembers.find(function(member) {
+    return member.district === district;
+  });
+
+  // Display the council member's picture, email, and phone number on the webpage
+  $("#picture").attr("src", councilMember.picture);
+  $("#name").text(councilMember.name);
+  $("#email").text(councilMember.email);
+  $("#phone").text(councilMember.phone);
+}
+
+// Load the map when the page is loaded
+$(document).ready(function() {
+  initMap();
+});
